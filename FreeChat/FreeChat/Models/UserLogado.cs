01192bb4844013dace1cae5c7717b507f45cf236
Daniel Rangel <danf.arangel@gmail.com>
2022-07-15 12:05:11 -0300

    first commit

---
+++ FreeChat/FreeChat/Models/UserLogado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeChat.Models
+{
+    public class UserLogado
+    {
+        public string token { get; set; }
+       
+    }
+}
---
+++ FreeChat/FreeChat/Services/MockDataStores/UserDataStores.cs
@@ -0,0 +1,60 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeChat.Services.MockDataStores
+{
+    public class UserDataStores : IDataStore<User>
+    {
+        readonly List<User> _users;
+
+        public UserDataStores()
+        {
+            _users = new List<User>();
+
+            _users.Add(new User("Lucas", "Henrique", "I like chatting online and making new friends on social media",
+                "logocosmospro.png", 5, 230)
+            { Id = Guid.NewGuid().ToString(), IsOnline = true });
+     
+        }
+
+        public Task<bool> AddItemAsync(User item)
+        {
+            _users.Add(item);
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> DeleteItemAsync(string id)
+        {
+            var user = _users.Where(u => u.Id == id);
+            if (!user.Any())
+                return Task.FromResult(false);
+            _users.Remove(user.FirstOrDefault());
+            return Task.FromResult(true);
+        }
+
+        public Task<User> GetItemAsync(string id)
+        {
+            return Task.FromResult(_users.Where(u => u.Id == id).FirstOrDefault());
+        }
+
+        public Task<IEnumerable<User>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return Task.FromResult((IEnumerable<User>)_users);
+        }
+
+        public Task<bool> UpdateItemAsync(User item)
+        {
+            var user = _users.Where(u => u.Id == item.Id);
+            if (!user.Any())
+                return Task.FromResult(false);
+
+            _users.Remove(_users.Where(u => u.Id == item.Id).FirstOrDefault());
+            _users.Add(item);
+            return Task.FromResult(true);
+        }
+    }
+}
